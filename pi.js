7b9fd267bdd0d1e992fc635660bb70fb7e2398b7
friendlysamurai <99750422+friendlysamurai@users.noreply.github.com>
2024-04-05 11:07:46 +0300

    Add files via upload

--- pi.js
+++ pi.js
@@ -1,3 +1,33 @@
-var now = new Date();
-var datetime = now.toLocaleString();
+let now = new Date();
+let datetime = now.toLocaleString();
 document.getElementById("datetime").innerHTML = datetime;
+
+let daudz = 80
+let sts = 0
+let zez = 0
+let ivd = [ievade.value]
+
+pi = [3.14159265358979323846264338327950288419716939937510582097494459230781640628620899]
+
+info.innerHTML = "π=3.1415926535 8979323846 2643383279 5028841971 6939937510 5820974944 5923078164 0628620899"
+
+function sakt (){
+    info.innerHTML = "-"
+    ez.innerHTML = "Tu atcrējies _ skaitļus!"
+}
+
+   function beigt (){
+    info.innerHTML = "π=3.1415926535 8979323846 2643383279 5028841971 6939937510 5820974944 5923078164 0628620899"
+    rez.innerHTML = "Tu atcrējies "+zez+" skaitļus!";
+   }
+   
+
+for (let i=0; i=daudz; i++) {
+if (pi[i] == ivd[i] ) {
+    zez = zez+1
+}
+else if (pi[i] != ivd[i]) {
+    daudz = i-1
+}
+
+}
