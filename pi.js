23e27d8348150c5bd09c90a79b408afbde096218
friendlysamurai <99750422+friendlysamurai@users.noreply.github.com>
2024-04-07 16:26:13 +0300

    Add files via upload

--- pi.js
+++ pi.js
@@ -2,32 +2,48 @@ let now = new Date();
 let datetime = now.toLocaleString();
 document.getElementById("datetime").innerHTML = datetime;
 
-let daudz = 80
-let sts = 0
-let zez = 0
-let ivd = [ievade.value]
+let sts = 0;
+let sks = 0;  
+let pi = "3.14159265358979323846264338327950288419716939937510582097494459230781640628620899"; 
+let daudz = pi.length;
+let beigt = function(){}
 
-pi = [3.14159265358979323846264338327950288419716939937510582097494459230781640628620899]
+info.innerHTML = "π = 3.1415926535 8979323846 2643383279 5028841971 6939937510 5820974944 5923078164 0628620899";
 
-info.innerHTML = "π=3.1415926535 8979323846 2643383279 5028841971 6939937510 5820974944 5923078164 0628620899"
-
-function sakt (){
-    info.innerHTML = "-"
-    ez.innerHTML = "Tu atcrējies _ skaitļus!"
+function sakt() {
+    info.innerHTML = "-";
+    rez.innerHTML = "Tu atcrējies _ skaitļus!";
+    sts = 1;
+    checkStatus();
 }
 
-   function beigt (){
-    info.innerHTML = "π=3.1415926535 8979323846 2643383279 5028841971 6939937510 5820974944 5923078164 0628620899"
-    rez.innerHTML = "Tu atcrējies "+zez+" skaitļus!";
-   }
-   
+function checkStatus() {
+    if (sts == 0) {
+        beigt = function() {}; 
+    } else if (sts == 1) {
+        beigt = function() {
+            info.innerHTML = "π = 3.1415926535 8979323846 2643383279 5028841971 6939937510 5820974944 5923078164 0628620899";
+            sks = 0; 
+            let iv = ievade.value; 
+            for (let i = 0; i < daudz; i++) {
+                if (iv[i] === pi[i]) {
+                    sks++;
+                    rez.innerHTML = "Tu atcrējies " + sks + " skaitļus!";
+                }
+            }
+        }
+    }
+}
 
-for (let i=0; i=daudz; i++) {
-if (pi[i] == ivd[i] ) {
-    zez = zez+1
-}
-else if (pi[i] != ivd[i]) {
-    daudz = i-1
-}
 
-}
+
+
+
+
+
+
+
+
+
+// document.write(ivd)
+// document.write(zez.innerHTML = ievade.value)
